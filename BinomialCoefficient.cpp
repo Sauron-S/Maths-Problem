e0647bb58f6a89492b3fdde6701b972162763f85
Saurabh Srivastava <46665920+Sauron-S@users.noreply.github.com>
2022-06-20 02:56:41 +0530

    Update BinomialCoefficient.cpp

--- BinomialCoefficient.cpp
+++ BinomialCoefficient.cpp
@@ -8,9 +8,9 @@ class Solution{
         pascalsTriangle.push_back(std::vector<int>({1}));
         for(int i=1;i<=N;i++){
             std::vector<int> row(i+1,0);
-            row[i][0] =1;row[i][i] =1;
+            row[0] =1;row[i] =1;
             for(int j=1;j<i;j++){
-                row[i][j] = row[i-1][j-1]+row[i-1][j];
+                row[j] = pascalsTriangle[i-1][j-1]+pascalsTriangle[i-1][j];
             }
             
             pascalsTriangle.push_back(std::move(row));
